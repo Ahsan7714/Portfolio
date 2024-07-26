a32c946b5b63841d354931b3f6f2057e6ef731f9
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2024-07-26 12:29:56 +0500

    .

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => (
       href='https://github.com/Ameer-Hamza289/portfolio'
       className='link footer__link' target="_blank"
     >
-    ©2023 All rights reserved - Ameer Hamza
+    ©2024 All rights reserved - Ahsan
     </a>
   </footer>
 )
